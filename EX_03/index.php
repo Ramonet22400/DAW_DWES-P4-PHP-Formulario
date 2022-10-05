6e6f9ab93438e4115dca0f63069183f09f8fdb38
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-05 13:42:23 +0200

    EX_02 acabado

--- EX_03/index.php
+++ EX_03/index.php
@@ -4,3 +4,8 @@
 //e imprime tantas líneas como te indiquen por el método POST y el mensaje:
 //“Escribiendo X líneas”. Dónde X es el número que nos han enviado.
 
+$numero = $_POST['numero'];
+for($i=0; $i <= $numero; $i++){
+    echo 'Escribiendo '.$i.'líneas.';
+    echo "<br>";
+}
